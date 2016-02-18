3dc6acf28b68415ee7e86cbee4c94cc20f09e81f
Ankur Chaudhry <ankurchaudhry.india@gmail.com>
2016-02-18 03:40:40 -0500

    initial timer file 2

--- timer/timer.c
+++ timer/timer.c
@@ -21,7 +21,14 @@ bool searchNode (struct node **head, int key) {
 }
 
 // update list
-
+void updateList(struct node *temp, int val,bool flag) {
+	while(temp && flag) {
+		temp->timeval -= val;
+	}
+	while(temp && !flag) {
+		temp->timeval += val;
+	}
+}
 
 // insert into node 
 void insertNode(struct node **head, struct node *temp) {
@@ -33,16 +40,44 @@ void insertNode(struct node **head, struct node *temp) {
 		if(temp1->timeval > val) {
 			temp->next = temp1;
 			*head = temp;
-			updateList((*head)->next,val);
+			updateList((*head)->next,val,true);
 		} else {
-			while(val > temp1->timeval) {
+			struct node *prev = temp1;
+			do{
 				val -= temp1->timeval;
 				temp1 = temp1->next;
-			}
-			
+			} while(temp1 && val >= temp1->timeval);
+			while(prev->next != temp1) prev = prev->next;
+			temp->next = prev->next;
+			prev->next = temp;
+			if(temp) 
+				updateList(temp1, val, true);
 		}
-		while(temp) {
-			if()
+	}
+}
+
+//delete a node
+bool deleteNode(struct node **head,int key) {
+	if(!(*head)) {
+		return false;
+	} else {
+		struct node *temp = *head;
+		if((*temp)->key == key) {
+			*head = (*head)->next;
+			updateList(*head,temp->timeval,false);
+			free(temp);
+		} else {
+			struct node *temp1 = *head;
+			while(temp && temp->key != key) {
+				temp = temp->next;
+			}
+			while(temp1->next != temp) {
+				temp1 = temp1->next;
+			}
+			temp1->next = temp->next;
+			temp->next = NULL;
+			updateList(temp1->next,temp->timeval,false);
+			free(temp);
 		}
 	}
 }
